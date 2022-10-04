58e93f66445a6d9ba7a670049a04fb2a2f03dd16
Ivan Grishin <brrdlam@Ivans-MacBook-Air.local>
2022-10-05 01:01:03 +0300

    Add past homework solutions

--- java-solutions/Sum.java
+++ java-solutions/Sum.java
@@ -1,4 +1,27 @@
 public class Sum {
     public static void main(String[] args) {
+        int sum = 0; // инициализируем сумму - будущий ответ
+        for (int i = 0; i < args.length; i++) {   
+            String stringPart = args[i]; // считываем часть входного аргумента
+            int startPos = 0; // флаг начала числа
+            int endPos = 0; // флаг конца числа
+            boolean flag = true; // костыль для суммы, если последнее число не сложилось :)
+            for (int j = 0; j < stringPart.length(); j++){
+                if ( Character.isWhitespace(stringPart.charAt(j)) ) {
+                    if (endPos != startPos) {
+                        sum += Integer.parseInt( stringPart.substring(startPos, endPos) ); // добавляем в сумму
+                    }
+                    startPos = j + 1; // двигаем начало строки на след символ 
+                    flag = false; // костыльный флаг
+                } else {
+                    flag = true; // костыльный флаг
+                }    
+                endPos = j + 1; // каждую итерацию двигаем конец строки    
+            }
+            if (flag) {
+                sum += Integer.parseInt( stringPart.substring(startPos, endPos) ); // добавляем в сумму
+            }
+        }
+        System.out.println(sum); // вывод получившейся суммы
     }
 }
--- java-solutions/WordStat.java
+++ java-solutions/WordStat.java
@@ -1,4 +1,68 @@
-public class WordStat {
+import java.util.*;
+import java.io.*;
+public class WordStatInput {
     public static void main(String[] args) {
+        Map <String, Integer> wordStat = new LinkedHashMap <String, Integer>();
+        try {
+            BufferedReader mainReader = new BufferedReader(new InputStreamReader(
+                new FileInputStream(args[0]),
+                "UTF-8"
+            ));
+            try {
+                String lineReader = mainReader.readLine();
+                while (lineReader != null) {
+                    int wordEndPos = 0;
+                    int wordStartPos = 0;
+                    String curWord = "";
+                    lineReader = lineReader.toLowerCase();
+                    for (int i = 0; i < lineReader.length(); i++) {
+                        if (Character.isLetter(lineReader.charAt(i))
+                        || Character.DASH_PUNCTUATION == Character.getType(lineReader.charAt(i))
+                        || lineReader.charAt(i) == '\'') {
+                            wordEndPos++;
+                        } else {
+                            if (wordEndPos != wordStartPos) { 
+                                curWord = lineReader.substring(wordStartPos, wordEndPos).trim();
+                                if (wordStat.containsKey(curWord)) {
+                                    int curCount = wordStat.get(curWord);
+                                    wordStat.put(curWord, wordStat.getOrDefolt(curWord, 0) + 1)
+                                }
+                            }
+                            wordEndPos = i + 1;
+                            wordStartPos = i + 1;
+                        }
+                    }
+                    if (wordEndPos != wordStartPos) { 
+                        curWord = lineReader.substring(wordStartPos, wordEndPos).trim();
+                        if (wordStat.containsKey(curWord)) {
+                            int curCount = wordStat.get(curWord);
+                            wordStat.put(curWord, wordStat.getOrDefolt(curWord, 0) + 1)
+                        }
+                    }
+                    lineReader = mainReader.readLine();
+                }
+            } finally {
+                mainReader.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Очень жаль, вы проиграли. " + e.getMessage());
+        } finally { 
+            try { 
+                BufferedWriter mainWriter = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(args[1]),
+                    "UTF-8"
+                ));
+                try { 
+                    for (Map.Entry entry : wordStat.entrySet()) {
+                        mainWriter.write(entry.getKey() + " " + entry.getValue());
+                        mainWriter.newLine();
+                    } 
+                } finally {
+                    mainWriter.close();
+                }
+            } catch (IOException e) { 
+                System.out.println("Очень жаль, вы проиграли. " + e.getMessage());
+            }
+        }
     }
 }
