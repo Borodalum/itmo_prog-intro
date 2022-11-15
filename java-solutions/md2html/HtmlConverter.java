2fdb1ab9c8b5673ff2ac14a45f321c6b9e42ab09
Ivan Grishin <brrdlam@Ivans-MacBook-Air.local>
2022-11-15 21:04:25 +0300

    Add Md2Html solution

---
+++ java-solutions/md2html/HtmlConverter.java
@@ -0,0 +1,147 @@
+package md2html;
+
+import java.lang.StringBuilder;
+import java.util.Map;
+import markup.*;
+
+public class HtmlConverter {
+    private final StringBuilder convertedHTML;
+    private final Map<String, String> alphabet = Map.of(
+            "*", "em", "_", "em", "**", "strong", "__", "strong",
+            "--", "s", "`", "code", "~", "mark",
+            "<", "&lt;", ">", "&gt;", "&", "&amp;"
+    );
+
+    public HtmlConverter() {
+        convertedHTML = new StringBuilder();
+    }
+
+    public void convertToHTML(StringBuilder currentToken) {
+        int startIndex = 0;
+        int headerLevel = 0;
+        while (currentToken.length() - 1 > startIndex && currentToken.charAt(startIndex) == '#') {
+            headerLevel++;
+            startIndex++;
+        }
+        if (currentToken.length() <= startIndex)
+            return;
+        if (!Character.isWhitespace(currentToken.charAt(startIndex))) {
+            startIndex -= headerLevel;
+            headerLevel = 0;
+        }
+        if (headerLevel == 0) {
+            convertedHTML.append("<p>");
+            parseInternal(currentToken.substring(startIndex));
+            convertedHTML.append("</p>").append(System.lineSeparator());
+        } else {
+            if (Character.isWhitespace(currentToken.charAt(startIndex))) {
+                startIndex++;
+            }
+            String openHeader = "<h" + headerLevel + ">";
+            String closeHeader = "</h" + headerLevel + ">" + System.lineSeparator();
+            convertedHTML.append(openHeader);
+            parseInternal(currentToken.substring(startIndex));
+            convertedHTML.append(closeHeader);
+        }
+    }
+
+    private void parseInternal(String bigToken) {
+        int i = 0;
+        StringBuilder buffer = new StringBuilder();
+        String curToken = "";
+        while (i < bigToken.length()) {
+            char token = bigToken.charAt(i);
+            if (token == '*' || token == '_') {
+                String temp;
+                if (i + 1 < bigToken.length() && bigToken.charAt(i + 1) == token) {
+                    temp = Character.toString(token).repeat(2);
+                    i++;
+                } else {
+                    temp = Character.toString(token);
+                }
+                if (curToken.equals(temp)) {
+                    convertedHTML.append("<").append(alphabet.get(curToken)).append(">");
+                    parseInternal(buffer.toString());
+                    convertedHTML.append("</").append(alphabet.get(curToken)).append(">");
+                    curToken = "";
+                    buffer.setLength(0);
+                } else if (curToken.isEmpty()){
+                    curToken = temp;
+                } else {
+                    buffer.append(temp);
+                }
+            } else if (token == '-') {
+                if (i + 1 < bigToken.length() && bigToken.charAt(i + 1) == token) {
+                    String temp = Character.toString(token).repeat(2);
+                    if (curToken.equals(temp)) {
+                        convertedHTML.append("<").append(alphabet.get(curToken)).append(">");
+                        parseInternal(buffer.toString());
+                        convertedHTML.append("</").append(alphabet.get(curToken)).append(">");
+                        curToken = "";
+                        buffer.setLength(0);
+                    } else if (curToken.isEmpty()) {
+                        curToken = temp;
+                    } else {
+                        buffer.append(temp);
+                    }
+                    i++;
+                } else {
+                    if (curToken.isEmpty()) {
+                        convertedHTML.append(token);
+                    } else {
+                        buffer.append(token);
+                    }
+                }
+            } else if (token == '`') {
+                String temp = Character.toString(token);
+                if (curToken.equals(temp)) {
+                    convertedHTML.append("<").append(alphabet.get(curToken)).append(">");
+                    parseInternal(buffer.toString());
+                    convertedHTML.append("</").append(alphabet.get(curToken)).append(">");
+                    curToken = "";
+                    buffer.setLength(0);
+                } else if (curToken.isEmpty()) {
+                    curToken = temp;
+                } else {
+                    buffer.append(temp);
+                }
+            } else if (token == '~') {
+                String temp = Character.toString(token);
+                if (curToken.equals(temp)) {
+                    convertedHTML.append("<").append(alphabet.get(curToken)).append(">");
+                    parseInternal(buffer.toString());
+                    convertedHTML.append("</").append(alphabet.get(curToken)).append(">");
+                    curToken = "";
+                    buffer.setLength(0);
+                } else if (curToken.isEmpty()) {
+                    curToken = temp;
+                } else {
+                    buffer.append(temp);
+                }
+            } else if (token == '\\') {
+                if (i + 1 < bigToken.length()) {
+                    if (curToken.isEmpty()) {
+                        convertedHTML.append(bigToken.charAt(i + 1));
+                    } else {
+                        buffer.append(bigToken.charAt(i + 1));
+                    }
+                    i++;
+                }
+            } else if ((token == '<' || token == '>' || token == '&') && curToken.isEmpty()){
+                convertedHTML.append(alphabet.get(Character.toString(token)));
+            } else if (curToken.isEmpty()) {
+                convertedHTML.append(token);
+            } else {
+                buffer.append(token);
+            }
+            i++;
+        }
+        if (buffer.length() != 0 || !curToken.isEmpty()) {
+            convertedHTML.append(curToken);
+            parseInternal(buffer.toString());
+        }
+    }
+    public String getConvertedHTML() {
+        return convertedHTML.toString();
+    }
+}
---
+++ java-solutions/md2html/Md2Html.java
@@ -0,0 +1,43 @@
+package md2html;
+
+import java.io.*;
+
+public class Md2Html {
+    public static void main(String[] args) {
+        try {
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(
+                new FileInputStream(args[0]),
+                    "UTF-8"
+            ));
+            BufferedWriter outputWriter = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(args[1]),
+                    "UTF-8"
+            ));
+            StringBuilder token = new StringBuilder();
+            HtmlConverter mdParser = new HtmlConverter();
+            // first read
+            String currentLine = inputReader.readLine();
+            while (currentLine != null) {
+                if (currentLine.trim().length() != 0) {
+                    token.append(currentLine);
+                    currentLine = inputReader.readLine();
+                    if (currentLine != null && currentLine.trim().length() != 0)
+                        token.append(System.lineSeparator());
+                    continue;
+                } else {
+                    mdParser.convertToHTML(token);
+                    token.setLength(0);
+                }
+                currentLine = inputReader.readLine();
+            }
+            mdParser.convertToHTML(token);
+            token.setLength(0);
+            outputWriter.write(mdParser.getConvertedHTML());
+            outputWriter.close();
+            inputReader.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
